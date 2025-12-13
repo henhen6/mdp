fbbbc9ec32d98ab0d92b701e08148ecfa22c5d2b
很很 <tangstruggle@gmail.com>
2025-12-13 23:51:31 +0800

    refactor(profile): 完善个人中心功能与用户信息更新接口
    - 新增 ProfileController，实现个人信息修改API
    - 新增 ProfileUserDto，定义个人信息数据结构与校验
    - 在 SsoUserService 接口及实现中增加更新个人信息方法
    - 实现用户头像关联文件的处理逻辑
    - 删除 LoginLogController 中的增加和更新接口，简化接口设计
    - LoginLogController 分页查询中完善设置创建者为当前用户id
    - 更改 AppApplyConvert 作者信息，维护代码注释一致性

--- mdp-platform/md-workbench/workbench-controller/src/main/java/top/mddata/workbench/controller/ProfileController.java
+++ mdp-platform/md-workbench/workbench-controller/src/main/java/top/mddata/workbench/controller/ProfileController.java
@@ -1,10 +1,43 @@
 package top.mddata.workbench.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import top.mddata.base.annotation.log.RequestLog;
+import top.mddata.base.base.R;
+import top.mddata.workbench.dto.ProfileUserDto;
+import top.mddata.workbench.service.SsoUserService;
+
 /**
  * 个人中心
+ *
  * @author henhen6
  * @since 2025/7/1 18:52
  */
+@Slf4j
+@RestController
+@RequestMapping("/profile")
+@AllArgsConstructor
+@Tag(name = "个人中心", description = "个人中心")
 public class ProfileController {
+    private final SsoUserService ssoUserService;
 
+    /**
+     * 修改个人信息
+     *
+     * @param dto 用户
+     * @return {@code true} 更新成功，{@code false} 更新失败
+     */
+    @PostMapping("/updateProfile")
+    @Operation(summary = "修改个人信息", description = "修改个人信息")
+    @RequestLog(value = "修改个人信息")
+    public R<Long> updateProfile(@Validated @RequestBody ProfileUserDto dto) {
+        return R.success(ssoUserService.updateProfile(dto));
+    }
 }
