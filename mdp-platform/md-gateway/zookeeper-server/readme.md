ae8d073edd56d6783719e8e1c9b2a3a0b22d2d94
很很 <tangstruggle@gmail.com>
2026-01-07 18:56:47 +0800

    feat(zookeeper-server): 新增内置简单注册中心模块

---
+++ mdp-platform/md-gateway/zookeeper-server/readme.md
@@ -0,0 +1,8 @@
+# zookeeper-server
+zookeeper-server 是内置简单的注册中心,基于zookeeper,用来开发演示使用.
+
+- 若项目需要启用开放平台全部功能，需要先启动 zookeeper-server 或 nacos
+- 然后在启动 sop-gateway-server 和 boot-server
+
+# 注意
+生产环境不可使用 zookeeper-server，请使用 nacos
---
+++ mdp-platform/md-gateway/zookeeper-server/src/main/java/top/mddata/registry/ZookeeperRegistryServer.java
@@ -0,0 +1,25 @@
+package top.mddata.registry;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 内置的建议注册中心 ，生产勿用
+ *
+ * @author henhen
+ */
+public class ZookeeperRegistryServer {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ZookeeperRegistryServer.class);
+
+    /**
+     * 内置简单的注册中心,基于zookeeper,用来开发演示使用.
+     *  生产环境不可使用!
+     * @param args 入参
+     */
+    public static void main(String[] args) {
+        int zkPort = 2181;
+        LOG.warn("启动内置zookeeper注册中心(仅在开发环境下使用),port={}", zkPort);
+        new EmbeddedZooKeeper(zkPort, false).start();
+    }
+}
