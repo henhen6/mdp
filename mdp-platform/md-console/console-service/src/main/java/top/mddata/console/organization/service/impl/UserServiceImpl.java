10fcc21dabc657486c0bfa57533184b48741b623
很很 <tangstruggle@gmail.com>
2026-01-07 18:58:18 +0800

    refactor(UserService): 优化导入语句，移除未使用的import

--- mdp-platform/md-console/console-service/src/main/java/top/mddata/console/organization/service/impl/UserServiceImpl.java
+++ mdp-platform/md-console/console-service/src/main/java/top/mddata/console/organization/service/impl/UserServiceImpl.java
@@ -49,7 +49,6 @@ import top.mddata.console.system.service.FileService;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
