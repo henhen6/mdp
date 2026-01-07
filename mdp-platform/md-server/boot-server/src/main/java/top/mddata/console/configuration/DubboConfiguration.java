f92ab3b811d21dd1cb27c968188424566a81b390
很很 <tangstruggle@gmail.com>
2026-01-07 18:58:12 +0800

    feat(configuration): 添加 Dubbo 配置及定时任务实现

---
+++ mdp-platform/md-server/boot-server/src/main/java/top/mddata/console/configuration/DubboConfiguration.java
@@ -0,0 +1,36 @@
+package top.mddata.console.configuration;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.scheduling.annotation.Scheduled;
+import top.mddata.open.admin.service.NotifyInfoService;
+
+import java.time.LocalDateTime;
+
+@Configuration
+// 仅当 spring.profiles.active = prod 时，该配置类才会被加载
+@Profile({"prod", "test", "sop"})
+//@ConditionalOnClass(DubboConfigConfiguration.class)
+@EnableDubbo
+@Slf4j
+public class DubboConfiguration {
+    public DubboConfiguration() {
+        log.info("dubbo 已加载");
+    }
+
+
+    @Autowired
+    private NotifyInfoService notifyInfoService;
+
+    /**
+     * 每分钟执行一次
+     */
+    @Scheduled(cron = "0 0/10 * * * ?")
+    public void run() {
+        notifyInfoService.retry(LocalDateTime.now());
+    }
+
+}
